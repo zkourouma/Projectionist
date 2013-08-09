5e8fcf21cedd461e9044e1c5483dbf6d5c6ce033
Zack Kourouma <zkourouma@gmail.com>
2013-08-09 13:58:34 -0400

    something

--- db/migrate/20130809173906_change_column_company_industries.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnCompanyIndustries < ActiveRecord::Migration
-  def change
-    change_column :company_industries, :company_id, :integer
-    change_column :company_industries, :industry_id, :integer
-  end
-end
